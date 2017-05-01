32a7cdfb0e4fd8695fd1e9e5c3ee407e19bcf96c
fenicesun <fenice.suns@gmail.com>
2017-05-01 21:13:03 +0800

    init project

---
+++ cmake-build-debug/src/CMakeFiles/fexchange.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fexchange.dir/main.cpp.o"
+  "fexchange.pdb"
+  "fexchange"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fexchange.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
